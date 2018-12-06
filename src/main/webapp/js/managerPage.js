cc2b5b16aa29cf37f0f2b42396f7f4b663e96695
YRJ <931622815@qq.com>
2018-12-06 09:29:12 +0800

    coding

--- src/main/webapp/js/managerPage.js
+++ src/main/webapp/js/managerPage.js
@@ -1,5 +1,5 @@
 var app = angular.module('managerPageApp', []);
 app.controller('managerPageCtrl', ['ngRoute'])
-    .controller('HomeController', function ($scope, $route) { $scope.$route = $route;})
-    .controller('AboutController', function ($scope, $route) { $scope.$route = $route;})
-    .config(function ($routeProvider)
+    .config(function ($routeProvider){
+        $routeProvider.when("/!proposerM");
+    }
